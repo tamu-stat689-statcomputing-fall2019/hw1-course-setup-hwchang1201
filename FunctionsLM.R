00110d485fb0648f0593ccfcdeaf3480d75dfbbb
Hyunwoong Chang <hwchang1201@tamu.edu>
2019-09-01 14:42:34 -0500

    apply R style guide to FunctionsLM.R

--- FunctionsLM.R
+++ FunctionsLM.R
@@ -7,22 +7,22 @@ generateY <- function(X, beta, sigma, seed = 5832652){
   
   #(exception) X should be 2-dimensional.
   
-  if(length(dim(X))>2) {
+  if(length(dim(X)) > 2) {
     stop("X is more than 2-dimensional") 
-  }else if(length(dim(X))>2) {
+  }else if(length(dim(X)) > 2) {
     stop("X is less than 2-dimensional")
   }
   
   #(exception) X and beta has to be compatible for matrix multiplication.
   
-  if(dim(X)[2]!=length(beta)) {
+  if(dim(X)[2] != length(beta)) {
     stop("the dimensions of X and beta are not compatible for matrix multiplication.") 
   }
   
   #[ToDo] Set seed and generate Y following linear model
   set.seed(seed)
-  epsilon <- sigma*rnorm(dim(X)[1])
-  Y <- X%*%beta + epsilon
+  epsilon <- sigma * rnorm(dim(X)[1])
+  Y <- X %*% beta + epsilon
 
   # Return Y
   return(Y)
@@ -35,20 +35,20 @@ calculateBeta <- function(X, Y){
   
   #(exception) X should be 2-dimensional.
   
-  if(length(dim(X))>2) {
+  if(length(dim(X)) > 2) {
     stop("X is more than 2-dimensional") 
-  }else if(length(dim(X))>2) {
+  }else if(length(dim(X)) > 2) {
     stop("X is less than 2-dimensional")
   }
   
   #(exception) t(X) and Y have to be compatible for matrix multiplication.
   
-  if (dim(X)[1]!=length(Y)){
+  if (dim(X)[1] != length(Y)){
     stop("t(X) and Y are not compatible for matrix multiplication")
   }
 
   #(t(X)%*%X is invertible)
-  beta_LS <-solve(crossprod(X,X),crossprod(X,Y))
+  beta_LS <-solve(crossprod(X, X), crossprod(X, Y))
 
   # Calculate beta_LS
   
@@ -63,11 +63,11 @@ calculateMSE <- function(beta, beta_LS){
   
   
   #(exception) beta and beta_LS have the same dimensions.
-  if (length(beta)!=length(beta_LS)) {
+  if (length(beta) != length(beta_LS)) {
     stop("beta and beta_LS have the different dimensions.")
   }
   
-  MSE <- as.numeric(crossprod(beta-beta_LS))
+  MSE <- as.numeric(crossprod(beta - beta_LS))
   
   # Return MSE - error ||beta - beta_LS||_2^2
   return(MSE)
